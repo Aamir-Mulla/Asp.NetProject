3c8166e8d26a459a8ac127a442f68ff485cd383b
Aamir-Mulla <aamirmulla93@gmail.com>
2019-04-23 13:55:02 +0530

    all files

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCrud.Models;
+using System.Data.SqlClient;
+
+namespace MvcCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        DbModels dbm = new DbModels();
+        public ActionResult Index()
+        {
+            var data = dbm.PRC_GETEMPDETAILS();
+            return View(data.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            SqlParameter param1 = new SqlParameter("@emp_id", id);
+            var data = dbm.Database.SqlQuery<Employee>("exec PRC_GETEMPBYID @emp_id", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                SqlParameter param1 = new SqlParameter("@name", employee.Name);
+                SqlParameter param2 = new SqlParameter("@address", employee.Address);
+                SqlParameter param3 = new SqlParameter("@position", employee.Position);
+                var data = dbm.Database.ExecuteSqlCommand("PRC_INSERTEMPDETAILS @name , @address , @position", param1, param2, param3);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            SqlParameter param1 = new SqlParameter("@emp_id", id);
+            var data = dbm.Database.SqlQuery<Employee>("exec PRC_GETEMPBYID @emp_id", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee employee)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                SqlParameter param1 = new SqlParameter("@emp_id", employee.EmpId);
+                SqlParameter param2 = new SqlParameter("@name", employee.Name);
+                SqlParameter param3 = new SqlParameter("@address", employee.Address);
+                SqlParameter param4 = new SqlParameter("@position", employee.Position);
+                var data = dbm.Database.ExecuteSqlCommand("PRC_UPDATEEMPDETAILS @emp_id , @name , @address , @position", param1, param2, param3, param4);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            SqlParameter param1 = new SqlParameter("@emp_id", id);
+            var data = dbm.Database.SqlQuery<Employee>("exec PRC_GETEMPBYID @emp_id", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                SqlParameter param1 = new SqlParameter("@emp_id", id);
+                dbm.Database.ExecuteSqlCommand("PRC_DELETEEMPDETAILS @emp_id", param1);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCrud.Models;
+using System.Data.SqlClient;
+
+namespace MvcCrud.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        DbModels dbModel = new DbModels();
+        public ActionResult Index()
+        {
+            var data = dbModel.prc_getstudentdetails();
+            return View(data.ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            SqlParameter param1 = new SqlParameter("studentId", id);
+            var data = dbModel.Database.SqlQuery<Student>("exec prc_getstudentbyid @studentId", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                SqlParameter param1 = new SqlParameter("@name", student.Name);
+                SqlParameter param2 = new SqlParameter("@class", student.Class);
+                SqlParameter param3 = new SqlParameter("@address", student.Address);
+                var data = dbModel.Database.ExecuteSqlCommand("prc_insertstudentdetails @name , @class , @address", param1, param2, param3);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            SqlParameter param1 = new SqlParameter("studentId", id);
+            var data = dbModel.Database.SqlQuery<Student>("exec prc_getstudentbyid @studentId", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student student)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                SqlParameter param1 = new SqlParameter("@studentId", student.StudentId);
+                SqlParameter param2 = new SqlParameter("@name", student.Name);
+                SqlParameter param3 = new SqlParameter("@class", student.Class);
+                SqlParameter param4 = new SqlParameter("@address", student.Address);
+                var data = dbModel.Database.ExecuteSqlCommand("prc_editstudent @studentId , @name , @class , @address", param1, param2, param3, param4);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            SqlParameter param1 = new SqlParameter("@studentId", id);
+            var data = dbModel.Database.SqlQuery<Student>("exec prc_getstudentbyid @studentId", param1).SingleOrDefault();
+            return View(data);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                SqlParameter param1 = new SqlParameter("@studentId", id);
+                var data = dbModel.Database.ExecuteSqlCommand("prc_deletestudentdetails @studentId", param1);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
